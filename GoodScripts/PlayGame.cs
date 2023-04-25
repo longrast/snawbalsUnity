f0563b5f8847ef424e92dd2df22e3536bff8b397
Fugu47 <godfather2002_15@mail.ru>
2023-04-25 23:16:29 +0300

    PauseMenu addded

--- GoodScripts/PlayGame.cs
+++ GoodScripts/PlayGame.cs
@@ -10,4 +10,8 @@ public class PlayGame : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
